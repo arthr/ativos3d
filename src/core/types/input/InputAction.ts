f59fa6e52879a29a83b0a802fc4f9d9324654f77
Arthur Morais <arthrmrs@gmail.com>
2025-08-31 20:05:30 -0300

    feat: define input action and mapping

---
+++ src/core/types/input/InputAction.ts
@@ -0,0 +1,12 @@
+/**
+ * Ações possíveis para eventos de input.
+ */
+export type InputAction =
+    | "select"
+    | "move"
+    | "place"
+    | "delete"
+    | "undo"
+    | "redo"
+    | "confirm"
+    | "cancel";
